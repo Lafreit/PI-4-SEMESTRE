682f953a6d808c5342e60e9ac8c8fd9feee2a8d8
Rayanne Nunes <nunesrayanne15@gmail.com>
2025-09-12 00:51:27 -0300

    Configurações iniciais

--- carona/settings.py
+++ carona/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Meus apps 
+    'pagamentos',
+    'reservas',
+    'usuarios',
+    'viagens',
+    'avaliacoes',
 ]
 
 MIDDLEWARE = [
@@ -115,8 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # para arquivos estáticos globais
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
