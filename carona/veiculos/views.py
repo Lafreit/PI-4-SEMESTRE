43ead02e337093f6b38deebb35753b31443ecfba
Sara Beatriz Bento <167379586+SaraBeatris@users.noreply.github.com>
2025-09-17 22:28:46 -0300

    Update views.py

--- carona/veiculos/views.py
+++ carona/veiculos/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
 
 # Create your views here.
+def cadastrar_veiculo(request):
+    """
+    View para exibir e processar o formulário de cadastro de veículos.
+    """
+    if request.method == 'POST':
+        marca = request.POST.get('marca')
+        modelo = request.POST.get('modelo')
+        ano = request.POST.get('ano')
+        placa = request.POST.get('placa')
+        
+    
+        
+
+        print(f"Dados recebidos do formulário de veículo:")
+        print(f"Marca: {marca}")
+        print(f"Modelo: {modelo}")
+        print(f"Ano: {ano}")
+        print(f"Placa: {placa}")
+        
+        return redirect('paginainicial')
+        
+    return render(request, 'cadastraveiculo.html')
